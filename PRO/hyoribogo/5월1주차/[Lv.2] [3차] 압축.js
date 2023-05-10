4b7a432ca0a640ec42689dcec495f12b04c14979
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-10 20:22:13 +0900

    230510

---
+++ PRO/hyoribogo/5월1주차/[Lv.2] [3차] 압축.js
@@ -0,0 +1,22 @@
+function solution(msg) {
+  const dict = [..."_ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  const result = []
+
+  while (msg.length) {
+    let w = ''
+    let idx = msg.length
+
+    for (; idx > 0; idx--) {
+      w = msg.slice(0, idx)
+
+      if (dict.includes(w)) {
+        result.push(dict.indexOf(w))
+        break
+      }
+    }
+    dict.push(w + msg[idx])
+    msg = msg.slice(w.length)
+  }
+
+  return result
+}
