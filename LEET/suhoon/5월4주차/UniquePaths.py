9cc88f7d2a1a4006d76f0a5fa398930ce04b3eab
suhoon <jobak4200nim@nate.com>
2023-05-29 21:50:07 +0900

    [1074]

--- LEET/suhoon/5월4주차/UniquePaths.py
+++ LEET/suhoon/5월4주차/UniquePaths.py
@@ -3,12 +3,15 @@ class Solution:
        
         dp ={}
         def dfs(r, c):
+            #base case
             if r == 1 or c == 1:
                 return 1
             
+            #memo
             if (r,c) not in dp:
+                #reculsive
                 dp[(r,c)] = dfs(r-1,c)+ dfs(r,c-1)
-            
+
             return dp[(r,c)]
         
         return(dfs(m,n))
