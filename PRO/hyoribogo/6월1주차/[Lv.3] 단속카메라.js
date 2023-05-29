0a1097a2f1742e50e3aa3630e1291982984a0591
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-29 20:10:37 +0900

    230529

---
+++ PRO/hyoribogo/6월1주차/[Lv.3] 단속카메라.js
@@ -0,0 +1,15 @@
+function solution(routes) {
+  routes.sort((a, b) => a[1] - b[1])
+  
+  let end = routes[0][1]
+  let result = 1
+  
+  for(const r of routes) {
+      if(end < r[0]) {
+          end = r[1]
+          result++
+      }
+  }
+  
+  return result
+}
