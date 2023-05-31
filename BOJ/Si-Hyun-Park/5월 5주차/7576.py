f33844a3cf2493916182788ebb5d51398492d1b4
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-05-31 20:58:53 +0900

    Update 7576.py

--- BOJ/Si-Hyun-Park/5월 5주차/7576.py
+++ BOJ/Si-Hyun-Park/5월 5주차/7576.py
@@ -4,30 +4,29 @@ m, n = map(int, input().split())
 tomato = [list(map(int, input().split())) for _ in range(n)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
+
 def bfs(tomato, m, n) :
   q = deque()
   
-  for i in range(m) :
-    for j in range(n) :
+  for i in range(n) :
+    for j in range(m) :
       if tomato[i][j] == 1 :
         q.append([i,j,0])
-  
   while q :
     x, y, d = q.popleft()
     for i in range(4) :
-      if 0 < x+dx[i] < m and 0 < y+dy[i] < n :
+      if (0 <= x+dx[i] < n )and (0 <= y+dy[i] < m) :
         if tomato[x+dx[i]][y+dy[i]] == 0 :
           q.append((x+dx[i],y+dy[i], d+1))
           tomato[x+dx[i]][y+dy[i]] = d+1
   lt = float('inf')
   mt = 0
   for l in tomato :
-    lt = min(min(l), lt)
     mt = max(max(l), mt)
-  
-  if lt == 0 :
-    return -1
-  else :
-    return mt
+    if 0 in l :
+      return -1
+  if mt == 1 :
+    return 0
+  return mt
 
-bfs(tomato, m, n)
+print(bfs(tomato, m, n))
