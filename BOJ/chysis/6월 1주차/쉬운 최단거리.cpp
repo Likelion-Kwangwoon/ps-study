410767db220b1ba2b0f2c68ecba69d262ea55329
chysis <chysiss@naver.com>
2023-06-06 20:45:26 +0900

    잘못 올린 코드 수정

--- BOJ/chysis/6월 1주차/쉬운 최단거리.cpp
+++ BOJ/chysis/6월 1주차/쉬운 최단거리.cpp
@@ -7,9 +7,10 @@ int dy[4]={0, 0, -1, 1};
 
 int N, M;
 int map[1001][1001];
+int ans[1001][1001];
 queue<pair<int, int>> q;
 
-void bfs(int x, int y){
+void bfs(){
     while(!q.empty()){
         pair<int, int> next = q.front();
         q.pop();
@@ -18,8 +19,8 @@ void bfs(int x, int y){
             int nx=next.first+dx[i];
             int ny=next.second+dy[i];
             
-            if(nx>=0 && nx<N && ny>=0 && ny<M && map[nx][ny]==1){
-                map[nx][ny]=map[next.first][next.second]+1;
+            if(nx>=0 && nx<N && ny>=0 && ny<M && ans[nx][ny]==-1 && map[nx][ny]==1){
+                ans[nx][ny]=ans[next.first][next.second]+1;
                 q.push({nx, ny});
             }
         }
@@ -28,22 +29,27 @@ void bfs(int x, int y){
 
 int main()
 {
-    int x, y;
     cin>>N>>M;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            ans[i][j]=-1;
+        }
+    }
+    
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin>>map[i][j];
             if(map[i][j]==2){
-                x=i; y=j;
-                q.push({x, y});
-                map[i][j]=0;
+                q.push({i, j});
+                ans[i][j]=0;
             }
         }
-    }   
-    bfs(x, y);
+    }
+    bfs();
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            cout<<map[i][j]<<" ";
+            if(map[i][j]==0) cout<<0<<" ";
+            else cout<<ans[i][j]<<" ";
         }
         cout<<"\n";
     }
