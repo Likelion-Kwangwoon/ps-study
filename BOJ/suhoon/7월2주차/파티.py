cc94403e1d8c763040e104cf754ebb0c53d915b0
suhoon <jobak4200nim@nate.com>
2023-07-13 22:06:57 +0900

    [1753]

--- BOJ/suhoon/7월2주차/파티.py
+++ BOJ/suhoon/7월2주차/파티.py
@@ -6,24 +6,21 @@ for i in range(M):
     a, b, c = map(int, sys.stdin.readline().split())
     graph[a].append((b,c))
 
-print(graph)
+#print(graph)
 INF = int(1e9)
 def dijkstra(start):
     distance = [INF] * (N+1)
     q = []
     heapq.heappush(q, (0,start))
     distance[start] = 0
-
     while(q):
-        dist, now = heapq.heappop(q)
-        # 이미 최단거리인지 확인
-        if distance[now] < dist:
-            continue
+        dist, now = heapq.heappop(q) #가장 거리가 작은걸 추출
         
-        for (next,weight) in graph[now]: #연결된 노드들 확인
-            if dist + weight < distance[next]: #더 작은 값이 있다면
-                distance[next] = dist + weight
-                heapq.heappush(q,(distance[next] ,next))
+        if distance[now] >= dist: #최단 거리가 아니라면
+            for (next,weight) in graph[now]: #연결된 노드들 확인
+                if dist + weight < distance[next]: #현재 저장되어있는 값보다 작다면
+                    distance[next] = dist + weight #업데이트
+                    heapq.heappush(q,(distance[next] ,next))
 
     return distance
 
