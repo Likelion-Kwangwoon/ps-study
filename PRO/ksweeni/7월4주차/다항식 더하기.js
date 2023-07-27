137b7bd306d817bdef772a2750591a24b597e0b3
ksweeni <ksween@naver.com>
2023-07-27 12:20:55 +0900

    230726, 230727

---
+++ PRO/ksweeni/7월4주차/다항식 더하기.js
@@ -0,0 +1,19 @@
+function solution(polynomial) {
+    const pol = polynomial.split(" + ");
+    let x_num = 0;
+    let num = 0;
+    let answer = [];
+    
+    pol.forEach((v) => {
+      if (v.includes("x")) {
+        const valtoNum = v.replace("x", "") || "1";
+        x_num += parseInt(valtoNum, 10);
+      } else {
+        num += Number(v);
+      }
+    });
+  
+    if (x_num) answer.push(`${x_num === 1 ? "" : x_num}x`);
+    if (num) answer.push(num);
+    return answer.join(" + ");
+  }
